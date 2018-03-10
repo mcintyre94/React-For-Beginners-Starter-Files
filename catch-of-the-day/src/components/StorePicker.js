c474af750fd1182dcb4b24032de0413a9d135e1a
mcintyre1994 <mcintyre1994@gmail.com>
2018-03-10 12:06:38 +0000

    Add event handling to StorePicker
    We change to /store/name-entered on submit, using React Router pushState

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,31 @@
 import React, { Fragment } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    const storeName = this.myInput.value.value;
+    // 3. Change the page to /store/store-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
+    // console.log(this);
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
