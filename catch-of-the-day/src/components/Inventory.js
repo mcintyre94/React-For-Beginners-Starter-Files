31cd250438fa8a1be920bf5fc48db9e182d102d0
mcintyre1994 <mcintyre1994@gmail.com>
2018-03-11 14:44:04 +0000

    Add Authentication to Inventory
    - Using Firebase Auth with Facebook, Twitter, Github login configured on Firebase app
    - New Login component shown in Inventory when no login
    - Owner stored in Firebase DB for each store
    - Inventory only shown to logged in user who is owner

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import firebase from "firebase";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
+import Login from "./Login";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -12,10 +15,76 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func.isRequired
   };
 
+  state = {
+    uid: null,
+    owner: null
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authHandler({ user });
+      }
+    });
+  }
+
+  authHandler = async authData => {
+    // 1. Look up the current store in firebase database
+    const store = await base.fetch(this.props.storeId, { context: this });
+    console.log(store);
+    // 2. Claim it if no owner
+    const uid = authData.user.uid;
+    if (!store.owner) {
+      // Save it as our own
+      await base.post(`${this.props.storeId}/owner`, {
+        data: uid
+      });
+    }
+    // 3. Set the state of inventory component to reflect current user
+    this.setState({
+      uid,
+      owner: store.owner || uid
+    });
+    console.log(authData);
+  };
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
+
+  logout = async () => {
+    console.log("Logging out!");
+    await firebase.auth().signOut();
+    this.setState({ uid: null });
+  };
+
   render() {
+    // 1. Check if user is logged in
+    if (!this.state.uid) {
+      return <Login authenticate={this.authenticate} />;
+    }
+
+    const logout = <button onClick={this.logout}>Log Out!</button>;
+
+    // 2. Check if logged in user is not the owner
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you are not the owner!</p>
+          {logout}
+        </div>
+      );
+    }
+
+    // 3. Logged in user is owner, render the inventory
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        {logout}
         {Object.keys(this.props.fishes).map(key => (
           <EditFishForm
             fish={this.props.fishes[key]}
